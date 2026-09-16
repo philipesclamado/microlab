cd8310f0c7f980e595792be0b857a0895e6d33d9
Philip Esclamado <philipesclamado@uvic.ca>
2026-09-16 12:05:11 -0600

    feat: add GPIO write

--- inc/gpio.h
+++ inc/gpio.h
@@ -4,59 +4,60 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef enum
-{
-    GPIO_OK = 0,
-    GPIO_ERROR_INVALID_PIN,
-    GPIO_ERROR_INVALID_CONFIG,
+typedef enum {
+  GPIO_OK = 0,
+  GPIO_ERROR_INVALID_PIN,
+  GPIO_ERROR_INVALID_CONFIG,
 } GPIO_Status;
 
-typedef enum
-{
-    GPIO_MODE_INPUT = 0U,
-    GPIO_MODE_OUTPUT = 1U,
+typedef enum {
+  GPIO_MODE_INPUT = 0U,
+  GPIO_MODE_OUTPUT = 1U,
 } GPIO_Mode;
 
-typedef enum
-{
-    GPIO_OUTPUT_TYPE_PUSHPULL = 0U,
-    GPIO_OUTPUT_TYPE_OPENDRAIN = 1U,
+typedef enum {
+  GPIO_OUTPUT_TYPE_PUSHPULL = 0U,
+  GPIO_OUTPUT_TYPE_OPENDRAIN = 1U,
 } GPIO_OutputType;
 
-typedef enum
-{
-    GPIO_SPEED_LOW = 0U,
-    GPIO_SPEED_HIGH = 3U,
+typedef enum {
+  GPIO_SPEED_LOW = 0U,
+  GPIO_SPEED_HIGH = 3U,
 } GPIO_OutputSpeed;
 
-typedef enum
-{
-    GPIO_PULL_NONE = 0U,
-    GPIO_PULL_UP = 1U,
-    GPIO_PULL_DOWN = 2U,
+typedef enum {
+  GPIO_PULL_NONE = 0U,
+  GPIO_PULL_UP = 1U,
+  GPIO_PULL_DOWN = 2U,
 } GPIO_Pull;
 
-typedef struct
-{
-    volatile uint32_t MODER;
-    volatile uint32_t OTYPER;
-    volatile uint32_t OSPEEDR;
-    volatile uint32_t PUPDR;
-    volatile uint32_t IDR;
-    volatile uint32_t ODR;
-    volatile uint32_t BSRR;
-    volatile uint32_t LCKR;
-    volatile uint32_t AFRL;
-    volatile uint32_t AFRH;
+typedef struct {
+  volatile uint32_t MODER;
+  volatile uint32_t OTYPER;
+  volatile uint32_t OSPEEDR;
+  volatile uint32_t PUPDR;
+  volatile uint32_t IDR;
+  volatile uint32_t ODR;
+  volatile uint32_t BSRR;
+  volatile uint32_t LCKR;
+  volatile uint32_t AFRL;
+  volatile uint32_t AFRH;
 } GPIO_TypeDef;
 
-// ref. https://www.st.com/resource/en/reference_manual/rm0091-stm32f0x1stm32f0x2stm32f0x8-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
+typedef enum { GPIO_LOW = 0U, GPIO_HIGH = 1U } GPIO_Level;
+
+// ref.
+// https://www.st.com/resource/en/reference_manual/rm0091-stm32f0x1stm32f0x2stm32f0x8-advanced-armbased-32bit-mcus-stmicroelectronics.pdf
 #define GPIOA ((GPIO_TypeDef *)0x48000000UL)
 #define GPIOB ((GPIO_TypeDef *)0x48000400UL)
 #define GPIOC ((GPIO_TypeDef *)0x48000800UL)
 
 #define RCC_AHBENR (*(volatile uint32_t *)(0x40021014UL))
 
-GPIO_Status GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode, uint8_t output_type, uint8_t output_speed, uint8_t pull_up_down);
+GPIO_Status GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode,
+                      uint8_t output_type, uint8_t output_speed,
+                      uint8_t pull_up_down);
+
+GPIO_Status GPIO_Write(GPIO_TypeDef *port, uint8_t pin, GPIO_Level level);
 
 #endif
--- src/drivers/gpio.c
+++ src/drivers/gpio.c
@@ -1,26 +1,38 @@
-#include <stdint.h>
 #include "gpio.h"
+#include <stdint.h>
 
-GPIO_Status GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode, uint8_t output_type, uint8_t output_speed, uint8_t pull_up_down)
-{
-    if (pin >= 16U)
-    {
-        return GPIO_ERROR_INVALID_PIN;
-    }
+GPIO_Status GPIO_Init(GPIO_TypeDef *port, uint8_t pin, uint8_t mode,
+                      uint8_t output_type, uint8_t output_speed,
+                      uint8_t pull_up_down) {
+  if (pin >= 16U) {
+    return GPIO_ERROR_INVALID_PIN;
+  }
 
-    uint32_t shift = pin * 2U;
+  uint32_t shift = pin * 2U;
 
-    port->MODER &= ~(3U << shift);
-    port->MODER |= ((uint32_t)mode << shift);
+  port->MODER &= ~(3U << shift);
+  port->MODER |= ((uint32_t)mode << shift);
 
-    port->OTYPER &= ~(1UL << pin);
-    port->OTYPER |= ((uint32_t)output_type << pin);
+  port->OTYPER &= ~(1UL << pin);
+  port->OTYPER |= ((uint32_t)output_type << pin);
 
-    port->OSPEEDR &= ~(3U << shift);
-    port->OSPEEDR |= ((uint32_t)output_speed << shift);
+  port->OSPEEDR &= ~(3U << shift);
+  port->OSPEEDR |= ((uint32_t)output_speed << shift);
 
-    port->PUPDR &= ~(3U << shift);
-    port->PUPDR |= ((uint32_t)pull_up_down << shift);
+  port->PUPDR &= ~(3U << shift);
+  port->PUPDR |= ((uint32_t)pull_up_down << shift);
 
-    return GPIO_OK;
+  return GPIO_OK;
+}
+
+GPIO_Status GPIO_Write(GPIO_TypeDef *port, uint8_t pin, GPIO_Level level) {
+  if (pin >= 16U)
+    return GPIO_ERROR_INVALID_PIN;
+
+  if (level == GPIO_HIGH)
+    port->ODR |= (1UL << pin);
+  else
+    port->ODR &= ~(1UL << pin);
+
+  return GPIO_OK;
 }
